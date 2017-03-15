bd595706d17f1c88d90168a47d95baff5afa42f0
TTeige <tim.teige91@gmail.com>
2017-03-15 13:04:34 +0100

    Added an abstract class which will be the interface for the scaling algorithms

---
+++ AutoscalingInterface.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tim on 15.03.17.
+//
+
+#include "AutoscalingInterface.h"
---
+++ AutoscalingInterface.h
@@ -0,0 +1,16 @@
+//
+// Created by tim on 15.03.17.
+//
+
+#ifndef MASTERPROJECT_AUTOSCALINGINTERFACE_H
+#define MASTERPROJECT_AUTOSCALINGINTERFACE_H
+
+
+class AutoscalingInterface {
+public:
+    virtual void doScale() = 0;
+
+};
+
+
+#endif //MASTERPROJECT_AUTOSCALINGINTERFACE_H
